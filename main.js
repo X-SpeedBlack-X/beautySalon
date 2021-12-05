c2123ba8000500be64bd228aac88df338ad639f6
X-SpeedBlack-X <isaqueblack129@gmail.com>
2021-12-05 15:01:54 -0300

    beautysalon

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 /* abre e fecha o menu quando clicar no ícone: hamburguer e X */
-const nav = document.querySelector('#header nav');
+const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
-for(const element of toggle) {
-  element.addEventListener('click', function() {
+for (const element of toggle) {
+  element.addEventListener('click', function () {
     nav.classList.toggle('show')
-  }) 
+  })
 }
 
 /* quando clicar em algum item no menu, esconder todo o menu */
 const links = document.querySelectorAll('nav ul li a')
 
-for(const link of links) {
-  link.addEventListener('click', function() {
+for (const link of links) {
+  link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
 }
 
 /* mudar o header da pagina quando der scroll */
-const header = document.querySelector("#header")
+const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
-  if(window.scrollY >= navHeight) {
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
     /* scroll é maior que altura do header  */
   } else {
@@ -30,3 +30,33 @@ window.addEventListener('scroll', function() {
     header.classList.remove('scroll')
   }
 })
+
+/* testimonials slide/carrousel swiper */
+/* const swiper = new swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+ */
+
+/* scrollReveal: mostrar elementos quando der scroll na página */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { internal: 100 }
+)
